236c3b0c5b068d6a72a809196f1fd3178fed9c4b
Piero Sabino <piero.sa@icloud.com>
2024-01-22 20:03:39 +0100

    Update project

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -29,7 +29,7 @@ const SearchForm: React.FC<SearchFormProps> = ({ formData, onSearch, onReset, se
         <div className="flex flex-wrap -mx-3 mb-0">
           <div className="w-full px-3 mb-6 md:mb-0">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="fullName">
-              Nome completo/Ragione sociale
+              Nome completo/ Ragione sociale
             </label>
             <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
                    type="text" 
